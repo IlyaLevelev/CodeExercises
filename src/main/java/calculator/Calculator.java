089596a6b122e59e0d0a4a41e3c81627c14b3d59
Ilya Levelev <ilja.levelev@gmail.com>
2018-09-11 23:37:24 +0200

    Calculator exercise created

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,34 @@
+package calculator;
+
+public class Calculator {
+
+	private double result;
+	
+	public Calculator (double result) {
+		this.result = 0;
+	}
+	
+	public void add (double value) {
+		this.result = this.result + value;
+	}
+	
+	public void subtract (double value) {
+		this.result = this.result - value;
+	}
+	
+	public double getResult () {
+		return this.result;
+	}
+	
+	public void multiply( double value) {
+		this.result = this.result * value;
+	}
+	
+	public void divide (double value) {
+		this.result = this.result / value;
+	}
+	
+	public void printStatus () {
+		System.out.println("The result is " + this.result);
+	}
+}
---
+++ src/main/java/calculator/Main.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculator result1 = new Calculator (5);
+		
+		result1.add(100);
+		result1.subtract(50);
+		result1.divide(2);
+		result1.multiply(4);
+		result1.printStatus();
+
+	}
+
+}
