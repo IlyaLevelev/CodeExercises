058d2f8b8bdc2d9631968f94308bd2b78dd46dec
Ilya Levelev <ilja.levelev@gmail.com>
2018-09-14 14:40:18 +0200

    Updated

--- src/main/java/statistics/Main.java
+++ src/main/java/statistics/Main.java
@@ -1,18 +1,19 @@
 package statistics;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		// create Statistics class
 		// it should have 2 fields: sum and count
-		Statistics stats = new Statistics();
-		
-		
-		stats.add(5);
-		stats.add(7);
-		stats.add(6);
-		stats.add(6);
+		Statistics stats = new Statistics(0, 0, null);
+			
+		numbers.add(5.0);
+		stats.add(7.0);
+		stats.add(6.0);
+		stats.add(6.0);
 		
 		double sum = stats.getSum(); // should be 24
 		double avg = stats.getAverage(); // should be 6
--- src/main/java/statistics/Statistics.java
+++ src/main/java/statistics/Statistics.java
@@ -1,11 +1,19 @@
 package statistics;
 
+import java.util.*;
+
 public class Statistics {
 
 	int sum;
 	int count;
+	List<Double> numbers;
 	
-	public Statistics () {
+	public Statistics (int sum, int count, List<Double> numbers) {
+		
+		this.sum = 0;
+		this.count = 0;
+		this.numbers = new ArrayList<Double>();
 	
 	}
+
 }
